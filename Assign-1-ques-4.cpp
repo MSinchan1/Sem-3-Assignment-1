d365aa4dddee53fc69d5d868ab51a5dc432a46cc
Sinchan Mitra <116640089+MSinchan1@users.noreply.github.com>
2023-07-23 22:07:44 +0530

    Update Assign-1-ques-4.cpp

--- Assign-1-ques-4.cpp
+++ Assign-1-ques-4.cpp
@@ -5,13 +5,13 @@ int main()
 	int i,n,sum=0,a=1,b=0,final_sum=1;//declaring variables
 	printf("Enter number: ");//taking input from the user for number of terms
 	scanf("%d",&n);
-	for(i=1;i<=n;i++)//iterates upto the number of terms
+	for(i=1;i<n;i++)//iterates upto the number of terms
 	{
 		sum=a+b;//0+1.......1+1.....2+1......3+2.....5+3......n
 		b=a;//1...1.....2......3......5
 		a=sum;//1...2.....3.....5......8
 		final_sum+=(sum*sum);//1+(1*1)+(3*3)+(5*5)+(8*8)
 	}
-	printf("%d		",final_sum);	
+	printf("Sum of the given series is: %d",final_sum);	
 	return 0;
 }
